2143023253cd529fd14d7ee1fe59e1e952f30bef
Adam Brunn <adambrunn1@hotmail.com>
2017-03-24 08:40:56 -0500

    Syncing Progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,28 @@
 var questionArr = [
-	new QuestionObj("question1", "Text1", ["answer1-1", "answer1-2"]), // 1 || index = 0
-	new QuestionObj("question2", "Text2", ["answer2-1", "answer2-2"]), // 2 || index = 1
-	new QuestionObj("question3", "Text3", ["answer3-1", "answer3-2"]), // 3 || index = 2
-	new QuestionObj("question4", "Text4", ["answer4-1", "answer4-2"])  // 4 || index = 3 || array.length = 4
+	new QuestionObj("question1", ["correct", "answer1-2", "answer1-3", "answer1-4"]), // 1 || index = 0
+	new QuestionObj("question2", ["correct", "answer2-2", "answer2-3", "answer2-4"]), // 2 || index = 1
+	new QuestionObj("question3", ["correct", "answer3-2", "answer3-3", "answer3-4"]), // 3 || index = 2
+	new QuestionObj("question4", ["correct", "answer4-2", "answer4-3", "answer4-4"])  // 4 || index = 3 || array.length = 4
 ];
 
-questionArr[questionArr.length-1]; // last element in an array
+var usedQuestionArr = [];
 
-var array = ["bread", "milk", "eggs", "turkey"];
+// questionArr[questionArr.length-1]; // last element in an array
 
-for(var i = 0; i<array.length; i++){
-	array[i];
-}
+// var array = ["bread", "milk", "eggs", "turkey"];
 
-var question1 = new QuestionObj("question1", "Text1", ["answer1-1", "answer1-2"]);
+// for(var i = 0; i<array.length; i++){
+// 	array[i];
+// }
+
+// var question1 = new QuestionObj("question1", "Text1", ["answer1-1", "answer1-2"]);
 
 // questionArr = Array.from(usedQuestionArr);
 // move questions from usedQuestionArr to questionArr;
-questionArr = usedQuestionArr.splice(0, usedQuestionArr.length);
+// questionArr = usedQuestionArr.splice(0, usedQuestionArr.length);
 
-function QuestionObj(question, correct, answers, time, img){
+function QuestionObj(question, answers, time, img){
 	this.question = question; // string
-	this.correctAnswer = correct;
 	this.answers = answers; // array
 	this.time = time || 30; // int - time in seconds
 	this.img = img || null;
@@ -29,15 +30,20 @@ function QuestionObj(question, correct, answers, time, img){
 }
 
 
-$("#button-div").on("click", function(event){ 
-	doStuff(target.event.id);
-});
+// $("#button-div").on("click", function(event){ 
+// 	doStuff(target.event.id);
+// });
+
+while(questionArr.length > 0){
+	grabRand("display");
+}
+
 
 function grabRand(type){
 	var index = Math.floor(Math.random()*questionArr.length);
-	var thisQuestionArr = [questionArr[index]];
-	usedQuestionArr.push(questionArr.splice(index, 1));
-	doStuff(type, thisQuestionArr);
+	var thisQuestion = [questionArr[index]];
+	usedQuestionArr.push(questionArr.splice(index, 1)[0]);
+	doStuff(type, thisQuestion);
 }
 
 function doStuff(type, thisQuestion){
@@ -45,21 +51,23 @@ function doStuff(type, thisQuestion){
 	var thisQuestion = thisQuestion || questionArr; 
 	var currentQuestion, currentAnswersArr;
 
-	for(var i = 0; i < questionArr.length; i++){
-		currentQuestion = questionArr[i].question;
-		currentAnswersArr = questionArr[i].answers;
+	for(var i = 0; i < thisQuestion.length; i++){
+		currentQuestion = thisQuestion[i].question;
+		currentAnswersArr = thisQuestion[i].answers;
 		if(type === "display"){
-			display();
+			display(currentQuestion, currentAnswersArr);
 		} else if(type === "reset"){
 			reset();
 		}
 	}
 }
 
-function display(){
-	console.log("Question: " + currentQuestion);
-	for(var i = 0; i < currentAnswersArr.length; i++){
-		console.log("-- Answer " + (i + 1) + ": " + currentAnswersArr[i]);
+function display(question, answersArr){
+	console.log("Question: " + question);
+	var loopNum = answersArr.length;
+	for(var i = 0; i < loopNum; i++){
+		var index = Math.floor(Math.random()*answersArr.length);
+		console.log("-- Answer " + (i + 1) + ": " + answersArr.splice(index, 1)[0]);
 	}
 	console.log("");
 }
@@ -67,3 +75,35 @@ function display(){
 function reset() {
 
 }
+
+/* To Select Random Array index and Not repeat */
+/*---------------------------------------------*/
+
+/* 	Test Variables:  */
+/* -----------------------*/
+
+//	var array = ["number 1", "number 2", "number 3", "number 4"];
+
+/*------------------------*/
+
+/*
+	// Store the total number of array elements
+	var loopNum = array.length;
+
+	// Create a new array (outArr) with the same values as the array
+	var outArr = Array.from(array);
+
+	// Loop the number of elements times
+	for(var i = 0; i < loopNum; i++){
+		
+		// Create a random index of outArr
+		var index = Math.floor(Math.random()*outArr.length);
+
+		// Remove and store value of the element at the random index 
+		var out = outArr.splice(index, 1)[0]
+
+		// Use the random array element as needed (Output)
+		console.log(out);
+
+	}
+*/

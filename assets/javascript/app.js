2b44c89766844ed6b33f1468352914f637a3a5e6
Adam Brunn <adambrunn1@hotmail.com>
2017-03-24 19:34:07 -0500

    Finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,17 @@
 var questionArr = [
-	new QuestionObj("question1", ["correct", "answer1-2", "answer1-3", "answer1-4"]), // 1 || index = 0
-	new QuestionObj("question2", ["correct", "answer2-2", "answer2-3", "answer2-4"]), // 2 || index = 1
-	// new QuestionObj("question3", ["correct", "answer3-2", "answer3-3", "answer3-4"]), // 3 || index = 2
-	// new QuestionObj("question4", ["correct", "answer4-2", "answer4-3", "answer4-4"])  // 4 || index = 3 || array.length = 4
+	new QuestionObj("What is Xanthophobia?", ["The fear of the color Yellow", "The fear of Crabs", "The fear of having too large of feet", "The fear of walking too fast"], "assets/images/yellow.jpg"),
+	new QuestionObj("What is Turophobia?", ["The fear of Cheese", "The fear of Renting Cars", "The fear of wearing Socks with Holes", "The fear of Bulls"], "assets/images/cheese.jpg"), 
+	new QuestionObj("What is Somniphobia?", ["The fear of Falling Asleep", "The fear of Crying in front of a Relative", "The fear of Sad Music", "The fear of Losing an Arm"], "assets/images/sleep.jpg"), 
+	new QuestionObj("What is Coulrophobia?", ["The fear of Clowns", "The fear of Vibrant Colors", "The fear of Wind", "The fear of Becoming Overweight"], "assets/images/clowns.jpg"),
+	new QuestionObj("What is Hylophobia?", ["The fear of Trees", "The fear of the Mood Swings", "The fear of the Periodic Table", "The fear of Sexual Kinks"], "assets/images/trees.jpg"),
+	new QuestionObj("What is Omphalophobia?", ["The fear of the Belly Button", "The fear of Slippery Surfaces", "The fear of Effort", "The fear of Midgets"], "assets/images/navel.jpg"),
+	new QuestionObj("What is Nomophobia?", ["The fear of having no Cell Phone Coverage", "The fear of Stopping", "The fear of AI Robots", "The fear of the Inside of the Knee"], "assets/images/phone.jpg"),
+	new QuestionObj("What is Ombrophobia?", ["The fear of Rain", "The fear of being Homeless", "The fear of Yawning", "The fear of having one's Neck Snapped from Behind"], "assets/images/rain.jpg"),
+	new QuestionObj("What is Papaphobia?", ["The fear of the Pope", "The fear of one's Significant Other's Father", "The fear of being Abandoned by Men", "The fear of Masculinity"], "assets/images/pope.jpg"),
+	new QuestionObj("What is Uranophobia?", ["The fear of Heaven", "The fear of Planetary Exploration", "The fear of Growing Up", "The fear of Peeing"], "assets/images/heaven.jpg"),
+	new QuestionObj("What is Pogonophobia?", ["The fear of Beards", "The fear of Jumping", "The fear of Arguments", "The fear of Having Regrets"], "assets/images/beards.jpg"),
+	new QuestionObj("What is Trypophobia?", ["The fear of Small Holes", "The fear of Dynosaurs", "The fear of Physical Harm", "The fear of Salt Water"], "assets/images/small_holes.jpg"),
+	new QuestionObj("What is Triskaidekaphobia?", ["The fear of the Number 13", "The fear of Open Doors", "The fear of Tripping down Stairs", "The fear of one's Birth Year"], "assets/images/number13.jpg"),
 ];
 
 var usedQuestionArr = [];
@@ -29,11 +38,11 @@ var numUnanswered = 0;
 // move questions from usedQuestionArr to questionArr;
 // questionArr = usedQuestionArr.splice(0, usedQuestionArr.length);
 
-function QuestionObj(question, answers, time, img){
+function QuestionObj(question, answers, img, time){
 	this.question = question; // string
 	this.answers = answers; // array
 	this.img = img || "assets/images/temp.png";
-	this.time = time || 10; // int - time in seconds
+	this.time = time || 20; // int - time in seconds
 }
 
 
@@ -60,7 +69,7 @@ function handleQuestion(thisQuestion){
 	// console.log("\n\n\n\n___________CLEAR____________\n\n\n\n");
 
 
-	console.log("____________________________");
+	// console.log("____________________________");
 	// console.log("Question: " + question);
 
 	
@@ -87,9 +96,9 @@ function handleQuestion(thisQuestion){
 	questionDiv.append(timeRemainTag);
 	questionDiv.append(questionTextTag);
 
-	console.log(questionDiv);
-	console.log("Correct Answer (zero-based): " + (answerKey));
-	console.log("____________END_____________");
+	// console.log(questionDiv);
+	// console.log("Correct Answer (zero-based): " + (answerKey));
+	// console.log("____________END_____________");
 
 	questionMasterDiv.empty();
 	questionMasterDiv.append(questionDiv);
@@ -179,6 +188,11 @@ function endGame() {
 
 function reset(){
 	questionArr = usedQuestionArr.splice(0);
+
+	numCorrect = 0;
+	numWrong = 0;
+	numUnanswered = 0;
+
 	grabRand();
 }
 
@@ -186,10 +200,8 @@ $( document ).ready(function(){
 	// Do stuff
 	questionMasterDiv = $("#question-master-div");
 
-	
-
 	questionMasterDiv.on("click", ".clickable", function(event){
-		console.log("CLICK DETECTED");
+		// console.log("CLICK DETECTED");
 		if($(this).hasClass("answer")){
 			if(parseInt($(this).attr("key")) === answerKey){
 				endQuestion("correct");
